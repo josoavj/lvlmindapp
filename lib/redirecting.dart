88f0adc0a83694be1dc49f030e2ff5b58d48bc98
Josoa <josoavonjiniaina13@gmail.com>
2023-10-22 09:53:27 +0400

    Modification dans la page Redirecting

--- lib/redirecting.dart
+++ lib/redirecting.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lvlmindbeta/animation.dart';
 import 'package:lvlmindbeta/presentation.dart';
+import 'package:flutter/gestures.dart';
 
 class Redirecting extends StatelessWidget {
   const Redirecting({super.key});
@@ -19,6 +21,78 @@ class Redirecting extends StatelessWidget {
           onPressed: () {
             Navigator.pop(context);
           },
+        ),
+      ),
+      // Juste un widget centré avec les deux logos suivi d'un lien de redirection
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            vertical: 100,
+            horizontal: 80,
+          ),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.center,
+              ),
+              SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    DelayedAnimation(
+                      delay: 100,
+                      child: Image(
+                          height: 100, image: AssetImage('images/ispm.jpg')),
+                    ),
+                    DelayedAnimation(
+                        delay: 100,
+                        child: SizedBox(
+                          height: 10,
+                        )),
+                    DelayedAnimation(
+                        delay: 100,
+                        child: Image(
+                          height: 100,
+                          image: AssetImage('images/lvlmind.jpg'),
+                        )),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                child: Row(
+                  // Placer les deux élements côte à côte
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize
+                      .min, // Pour diminuer l'espacement entre les deuc élements
+                  children: [
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(
+                        fontFamily: 'Josefin',
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "Link",
+                      style: TextStyle(
+                        fontFamily: 'Josefin',
+                        color: Colors.blueAccent,
+                        fontSize: 20,
+                        decoration: TextDecoration.underline,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
