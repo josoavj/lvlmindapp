bb595b4baada68ac499e41164a462fc7b24e403b
Fiatoa <falyfiatoa@gmail.com>
2023-10-22 20:34:05 +0300

    background du presentation.dart

--- lib/presentation.dart
+++ lib/presentation.dart
@@ -14,6 +14,7 @@ class Presentation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white.withOpacity(0),
@@ -33,149 +34,172 @@ class Presentation extends StatelessWidget {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            DelayedAnimation(
-              delay: 1200,
-              child: SizedBox(
-                height: 230,
-                child: Image(
-                  image: AssetImage('images/lvlmind.jpg'),
-                ), //Logo de notre application
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            vertical: 100,
+            horizontal: 50,
+          ),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xff2441e7), Color(0xffff1053)],
+            ),
+            image: DecorationImage(
+              image: const AssetImage('images/fond1 1.jpg'),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.5),
+                BlendMode.dstATop,
               ),
             ),
-            DelayedAnimation(
-              delay: 1300,
-              child: Text(
-                "LVLMIND",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Adlam',
-                  fontSize: 30,
-                  fontWeight: FontWeight.w600,
-                  color: Color.fromARGB(255, 82, 7, 186),
+          ),
+          child: Column(
+            children: [
+              DelayedAnimation(
+                delay: 1200,
+                child: SizedBox(
+                  height: 230,
+                  child: Image(
+                    image: AssetImage('images/lvlmind.jpg'),
+                  ), //Logo de notre application
                 ),
               ),
-            ),
-            DelayedAnimation(
-              delay: 1700,
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 40,
-                  horizontal: 30,
-                ),
-                child: Column(
-                  children: const [
-                    Text(
-                      "Your adventure begins here and now.",
-                      style: TextStyle(
-                        fontFamily: 'Josefin',
-                        fontSize: 30,
-                        fontWeight: FontWeight.w600,
-                        color: Color.fromARGB(255, 16, 162, 236),
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Train your mind to gain more knowledge with levelmind",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontFamily: 'Josefin',
-                        fontSize: 20,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.blueAccent,
-                      ),
-                    ),
-                  ],
+              DelayedAnimation(
+                delay: 1300,
+                child: Text(
+                  "LVLMIND",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Adlam',
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                    color: Color.fromARGB(255, 82, 7, 186),
+                  ),
                 ),
               ),
-            ),
-            DelayedAnimation(
-              delay: 2500,
-              child: Container(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 15,
-                  horizontal: 60,
-                ),
-                child: Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => LoginPage(),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: const StadiumBorder(),
-                        backgroundColor: Colors.blueAccent,
-                        padding: const EdgeInsets.all(13),
+              DelayedAnimation(
+                delay: 1700,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 40,
+                    horizontal: 30,
+                  ),
+                  child: Column(
+                    children: const [
+                      Text(
+                        "Your adventure begins here and now.",
+                        style: TextStyle(
+                          fontFamily: 'Josefin',
+                          fontSize: 30,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromARGB(255, 16, 162, 236),
+                        ),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          //Icon(Icons.send_sharp), //A modifier
-                          SizedBox(width: 10),
-                          Text(
-                            'Connect with Personal ID',
-                            style: TextStyle(
-                              fontFamily: 'Josefin',
-                              fontSize: 15,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
+                      SizedBox(height: 10),
+                      Text(
+                        "Train your mind to gain more knowledge with levelmind",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Josefin',
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              DelayedAnimation(
+                delay: 2500,
+                child: Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 60,
+                  ),
+                  child: Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginPage(),
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 15),
-                    // Texte avec lien de redirection dans une page redirecting
-                    SizedBox(
-                      child: Row(
-                        // Placer les deux élements côte à côte
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        mainAxisSize: MainAxisSize
-                            .min, // Pour diminuer l'espacement entre les deuc élements
-                        children: [
-                          Text(
-                            "Don't have an account?",
-                            style: TextStyle(
-                              fontFamily: 'Josefin',
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: const StadiumBorder(),
+                          backgroundColor: Colors.blueAccent,
+                          padding: const EdgeInsets.all(13),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            //Icon(Icons.send_sharp), //A modifier
+                            SizedBox(width: 10),
+                            Text(
+                              'Connect with Personal ID',
+                              style: TextStyle(
+                                fontFamily: 'Josefin',
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                              ),
                             ),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => Redirecting(),
-                                ),
-                              );
-                            },
-                            child: const DelayedAnimation(
-                              delay: 2500,
-                              child: Text("Register",
-                                  style: TextStyle(
-                                    fontFamily: 'Josefin',
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.blueAccent,
-                                    decoration: TextDecoration.underline,
-                                  )),
-                            ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: 15),
+                      // Texte avec lien de redirection dans une page redirecting
+                      SizedBox(
+                        child: Row(
+                          // Placer les deux élements côte à côte
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize
+                              .min, // Pour diminuer l'espacement entre les deuc élements
+                          children: [
+                            Text(
+                              "Don't have an account?",
+                              style: TextStyle(
+                                fontFamily: 'Josefin',
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Redirecting(),
+                                  ),
+                                );
+                              },
+                              child: const DelayedAnimation(
+                                delay: 2500,
+                                child: Text("Register",
+                                    style: TextStyle(
+                                      fontFamily: 'Josefin',
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.blueAccent,
+                                      decoration: TextDecoration.underline,
+                                    )),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
