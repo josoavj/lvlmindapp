221d39efb16d30f32cc7558e24f799b753984d03
Josoa <josoavonjiniaina13@gmail.com>
2023-11-01 08:01:28 +0400

    Courses list building

--- lib/Models/Courses.dart
+++ lib/Models/Courses.dart
@@ -1,1 +1,18 @@
+class CoursesModels {
+  String name; // Pour le nom de la matière
 
+  // Création du constructeur pour la classe
+  CoursesModels({
+    required this.name,
+  });
+
+  List<CoursesModels> getCoursesModels() {
+    List<CoursesModels> courses = [];
+    courses.add(CoursesModels(name: "Electronic"));
+    courses.add(CoursesModels(name: "AI"));
+    courses.add(CoursesModels(name: "Data Structure"));
+    courses.add(CoursesModels(name: "Algorithm"));
+    courses.add(CoursesModels(name: ""));
+    return courses;
+  }
+}
