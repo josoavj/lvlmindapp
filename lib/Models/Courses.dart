376f323c3931c5a1e79178c21266809fa520a135
Josoa <josoavonjiniaina13@gmail.com>
2023-11-01 11:04:50 +0400

    Update Courses.dart

--- lib/Models/Courses.dart
+++ lib/Models/Courses.dart
@@ -6,13 +6,32 @@ class CoursesModels {
     required this.name,
   });
 
-  List<CoursesModels> getCoursesModels() {
+  static List<CoursesModels> getCoursesModels() {
     List<CoursesModels> courses = [];
     courses.add(CoursesModels(name: "Electronic"));
     courses.add(CoursesModels(name: "AI"));
     courses.add(CoursesModels(name: "Data Structure"));
     courses.add(CoursesModels(name: "Algorithm"));
-    courses.add(CoursesModels(name: ""));
+    courses.add(CoursesModels(name: "UI/UX"));
+    courses.add(CoursesModels(name: "Project Management"));
     return courses;
   }
 }
+
+/*
+class Topics extends CoursesModels {
+  String tname;
+  String category;
+
+  Topics({
+    required this.tname,
+    required this.category,
+    required super.name,
+  });
+
+  static List<Topics> getTopics() {
+    List<Topics> topics = [];
+    topics.add(Topics(tname: "Chapter 1", category: "MSI", name: "Electronic"));
+  }
+}
+*/
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,41 +1,47 @@
 // ignore_for_file: no_logic_in_create_state, unused_import, avoid_unnecessary_containers
-
 import 'package:flutter/material.dart';
 import 'package:lvlmindbeta/main.dart';
 import 'package:lvlmindbeta/animation.dart';
 import 'package:lvlmindbeta/loginpage.dart';
+import 'package:lvlmindbeta/Models/Courses.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
-  // ignore: avoid_types_as_parameter_names, non_constant_identifier_names
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
+    List<CoursesModels> courses = [];
     return Scaffold(
       extendBodyBehindAppBar: false,
       appBar: AppBar(),
       body: Container(
-        alignment: Alignment.center,
-        child: const Column(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               mainAxisSize: MainAxisSize.min,
               children: [
+                Padding(
+                    padding: EdgeInsets.only(
+                  top: 30,
+                  left: 10,
+                  right: 10,
+                )),
                 Text.rich(TextSpan(
-                    text: "Hi honey \n",
+                    text: "Hi there \n",
                     style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Josefin',
+                      color: Colors.blueGrey,
+                      fontFamily: 'Adlam',
                       fontSize: 25,
                       fontWeight: FontWeight.w700,
                     ),
                     children: [
                       TextSpan(
-                        text: "Let's start a day exciting while learning",
+                        text: "Let's start a day exciting \n while learning",
                         style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Josefin',
+                          color: Colors.blueGrey,
+                          fontFamily: 'Adlam',
                           fontSize: 15,
                           fontWeight: FontWeight.w300,
                         ),
@@ -45,16 +51,95 @@ class Homepage extends StatelessWidget {
                   width: 20,
                 ),
                 SizedBox(
-                  width: 80,
+                  width: 90,
                   child: Image(
                     image: AssetImage('images/logomin.jpg'),
                   ),
                 ),
               ],
-            )
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: courses.length,
+                  padding: const EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    return ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        courses[index].name,
+                        style: const TextStyle(
+                          fontFamily: 'Josefin',
+                          fontWeight: FontWeight.w300,
+                          color: Color.fromARGB(255, 99, 98, 98),
+                          fontSize: 15,
+                        ),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) => const SizedBox(
+                        height: 10,
+                      )),
+            ),
+            const Courseslist(),
+            const SizedBox(
+              height: 10,
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class Courseslist extends StatefulWidget {
+  const Courseslist({super.key});
+
+  @override
+  CourseslistState createState() => CourseslistState();
+}
+
+class CourseslistState extends State<Courseslist> {
+  @override
+  Widget build(BuildContext context) {
+    List<CoursesModels> courses = [];
+    /*
+    void _getCoursesModels() {
+    courses = CoursesModels.getCoursesModels();
+    }
+     */
+    return SizedBox(
+      height: 30,
+      child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          itemCount: courses.length,
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                courses[index].name,
+                style: const TextStyle(
+                  fontFamily: 'Josefin',
+                  fontWeight: FontWeight.w300,
+                  color: Color.fromARGB(255, 99, 98, 98),
+                  fontSize: 15,
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) => const SizedBox(
+                height: 10,
+              )),
     );
   }
 }
