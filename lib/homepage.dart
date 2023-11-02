498937dcba3956adddc8affd07e287cc21cd7c35
Josoa <josoavonjiniaina13@gmail.com>
2023-11-02 17:24:14 +0400

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -182,7 +182,24 @@ class Category extends StatefulWidget {
 class CategoryState extends State<Category> {
   @override
   Widget build(BuildContext context) {
+    return GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2, crossAxisSpacing: 10),
+        itemCount: 8,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.rectangle,
                   ),
+                ),
+                const Text("Program"),
+              ],
+            ),
+          );
+        });
   }
 }
