7279e64201e28343b67a0a970181d5ce21da2ec2
Josoa <76913187+Josoa886@users.noreply.github.com>
2023-11-23 21:10:28 +0400

    Modification de la barre de navigation

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,37 +8,39 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: ClipRRect(
-        // Pour obtenir des bords arrondis
-        borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-        child: NavigationBar(
-            onDestinationSelected: (index) => {},
-            backgroundColor: const Color.fromARGB(148, 55, 188, 255),
-            elevation: 0,
-            height: 75,
-            selectedIndex: 0,
-            indicatorColor: const Color.fromARGB(255, 255, 255, 255),
-            //onDestinationSelected: (index) => ,
-            destinations: const [
-              NavigationDestination(
-                icon: Icon(Iconsax.home_2),
-                label: 'Home',
-              ),
-              NavigationDestination(icon: Icon(Iconsax.calendar), label: 'EDT'),
-              NavigationDestination(
-                icon: Icon(Iconsax.folder_2),
-                label: 'Files',
-                /*
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Files()));
-                },
-                */
-              ),
-              NavigationDestination(
-                  icon: Icon(Iconsax.profile_circle), label: 'Profile')
-            ]),
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.5),
+              blurRadius: 25,
+              offset: const Offset(0, 20))
+        ]),
+        child: ClipRRect(
+          // Pour obtenir des bords arrondis
+          borderRadius: BorderRadius.circular(30),
+          child: NavigationBar(
+              onDestinationSelected: (index) => {},
+              backgroundColor: const Color.fromARGB(148, 55, 188, 255),
+              elevation: 0,
+              height: 75,
+              selectedIndex: 0,
+              indicatorColor: const Color.fromARGB(255, 255, 255, 255),
+              destinations: const [
+                NavigationDestination(
+                  icon: Icon(Iconsax.home_2),
+                  label: 'Home',
+                ),
+                NavigationDestination(
+                    icon: Icon(Iconsax.calendar), label: 'EDT'),
+                NavigationDestination(
+                  icon: Icon(Iconsax.folder_2),
+                  label: 'Files',
+                ),
+                NavigationDestination(
+                    icon: Icon(Iconsax.profile_circle), label: 'Profile')
+              ]),
+        ),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(
