6671cd0eddc2a3fac8528d371d2920053faf56eb
Josoa <76913187+Josoa886@users.noreply.github.com>
2023-11-25 11:11:03 +0400

    Clean code for the login page

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -14,46 +14,7 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        automaticallyImplyLeading: false,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Flexible(
-              flex: 1,
-              child: IconButton(
-                icon: const Icon(
-                  Icons.close,
-                  color: Colors.blueAccent,
-                  size: 25,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const Presentation(),
-                      ));
-                },
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Flexible(
-                flex: 2,
-                child: Padding(
-                    padding: EdgeInsets.only(top: 10),
-                    child: Image(
-                      height: 50,
-                      image: AssetImage('images/logo/logomin.jpg'),
-                    )))
-          ],
-        ),
-        titleSpacing: 0,
-      ),
+      appBar: appbar(context),
       body: Stack(
         children: [
           Align(
@@ -82,132 +43,181 @@ class LoginPage extends StatelessWidget {
                 'images/decoration/Ellipse.svg',
                 width: 200,
               )),
-          SingleChildScrollView(
-            child: Column(
+          loginbody(context),
+        ],
+      ),
+    );
+  }
+
+  // Barre d'application
+  AppBar appbar(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      automaticallyImplyLeading: false,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Flexible(
+            flex: 1,
+            child: IconButton(
+              icon: const Icon(
+                Icons.close,
+                color: Colors.blueAccent,
+                size: 25,
+              ),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Presentation(),
+                    ));
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Flexible(
+              flex: 2,
+              child: Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Image(
+                    height: 50,
+                    image: AssetImage('images/logo/logomin.jpg'),
+                  )))
+        ],
+      ),
+      titleSpacing: 0,
+    );
+  }
+
+  // login
+  SingleChildScrollView loginbody(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 100,
+              horizontal: 30,
+            ),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 100,
-                    horizontal: 30,
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: DelayedAnimation(
+                    delay: 800,
+                    child: Text(
+                      'Connect to levelmind',
+                      style: TextStyle(
+                        fontFamily: 'PatrickHand',
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
                   ),
-                  child: const Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: DelayedAnimation(
-                          delay: 800,
-                          child: Text(
-                            'Connect to levelmind',
-                            style: TextStyle(
-                              fontFamily: 'PatrickHand',
-                              fontSize: 30,
-                              fontWeight: FontWeight.w700,
-                            ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: SizedBox(height: 22),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: DelayedAnimation(
+                      delay: 850,
+                      child: Flexible(
+                        flex: 1,
+                        child: Text(
+                          "It's recommended to connect",
+                          style: TextStyle(
+                            fontFamily: 'Josefin',
+                            fontSize: 20,
+                            color: Color.fromARGB(95, 15, 12, 199),
                           ),
                         ),
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: SizedBox(height: 22),
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: DelayedAnimation(
-                            delay: 850,
-                            child: Flexible(
-                              flex: 1,
-                              child: Text(
-                                "It's recommended to connect",
-                                style: TextStyle(
-                                  fontFamily: 'Josefin',
-                                  fontSize: 20,
-                                  color: Color.fromARGB(95, 15, 12, 199),
-                                ),
-                              ),
-                            )),
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: DelayedAnimation(
-                            delay: 850,
-                            child: Flexible(
-                              flex: 1,
-                              child: Text(
-                                "with your ID.",
-                                style: TextStyle(
-                                  fontFamily: 'Josefin',
-                                  fontSize: 20,
-                                  color: Color.fromARGB(95, 15, 12, 199),
-                                ),
-                              ),
-                            )),
-                      ),
-                    ],
-                  ),
+                      )),
                 ),
-                const SizedBox(height: 0),
-                const LoginForm(),
-                const SizedBox(height: 50),
-                DelayedAnimation(
-                  delay: 1100,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: const StadiumBorder(),
-                      // J'ai utilisé primary mais elle est obsolète
-                      // Du coup je l'ai remplacé par backgroundColor
-                      backgroundColor: const Color.fromARGB(225, 249, 29, 88),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 90,
-                        vertical: 15,
-                      ),
-                    ),
-                    child: const Text(
-                      'CONFIRM',
-                      style: TextStyle(
-                        fontFamily: 'Josefin',
-                        color: Colors.black,
-                      ),
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Homepage(),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                const SizedBox(height: 90),
                 Align(
-                  alignment: Alignment.centerRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 35),
-                    child: TextButton(
-                        onPressed: () {
-                          Navigator.pop(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const Presentation(),
-                              ));
-                        },
-                        child: const DelayedAnimation(
-                          delay: 1100,
-                          child: Flexible(
-                              flex: 1,
-                              child: Text(
-                                "Return",
-                                style: TextStyle(
-                                    fontFamily: 'Josefin',
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.blueAccent),
-                              )),
-                        )),
-                  ),
+                  alignment: Alignment.centerLeft,
+                  child: DelayedAnimation(
+                      delay: 850,
+                      child: Flexible(
+                        flex: 1,
+                        child: Text(
+                          "with your ID.",
+                          style: TextStyle(
+                            fontFamily: 'Josefin',
+                            fontSize: 20,
+                            color: Color.fromARGB(95, 15, 12, 199),
+                          ),
+                        ),
+                      )),
                 ),
               ],
+            ),
+          ),
+          const SizedBox(height: 0),
+          const LoginForm(),
+          const SizedBox(height: 50),
+          DelayedAnimation(
+            delay: 1100,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+                // J'ai utilisé primary mais elle est obsolète
+                // Du coup je l'ai remplacé par backgroundColor
+                backgroundColor: const Color.fromARGB(225, 249, 29, 88),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 90,
+                  vertical: 15,
+                ),
+              ),
+              child: const Text(
+                'CONFIRM',
+                style: TextStyle(
+                  fontFamily: 'Josefin',
+                  color: Colors.black,
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Homepage(),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 90),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 35),
+              child: TextButton(
+                  onPressed: () {
+                    Navigator.pop(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Presentation(),
+                        ));
+                  },
+                  child: const DelayedAnimation(
+                    delay: 1100,
+                    child: Flexible(
+                        flex: 1,
+                        child: Text(
+                          "Return",
+                          style: TextStyle(
+                              fontFamily: 'Josefin',
+                              fontSize: 15,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.blueAccent),
+                        )),
+                  )),
             ),
           ),
         ],
