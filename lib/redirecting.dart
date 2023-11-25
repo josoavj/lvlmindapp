2ce9b5c382c23ad300dfb6d91cb4dd67fdad3e19
Josoa <76913187+Josoa886@users.noreply.github.com>
2023-11-25 19:31:47 +0400

    .

--- lib/redirecting.dart
+++ lib/redirecting.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/gestures.dart';
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:lvlmindbeta/animation.dart';
 import 'package:simple_gradient_text/simple_gradient_text.dart';
