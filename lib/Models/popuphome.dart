7a2b2197892f677462a45e16902fefc5bbd6b44e
Josoa <josoavonjiniaina13@gmail.com>
2025-07-05 11:04:36 +0300

    update: Correction du popup dans Homepage

--- lib/Models/popuphome.dart
+++ lib/Models/popuphome.dart
@@ -3,17 +3,35 @@ import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class Section {
   final IconData icon;
-  String name;
+  final String name; // Changed to final as 'name' usually doesn't change after creation.
+  final VoidCallback? onTap; // Added an optional callback for when the section is tapped.
 
-  Section({
+  const Section({
     required this.icon,
     required this.name,
+    this.onTap, // Made onTap optional
   });
 
-  static List<Section> getSection() {
-    List<Section> secteur = [];
-    secteur.add(Section(icon: Iconsax.add_square, name: "Add"));
-    secteur.add(Section(icon: Iconsax.pen_add, name: "Modify"));
-    return secteur;
+  // Provides a static list of predefined sections.
+  // This can be easily extended or modified.
+  static List<Section> getSections() {
+    return [
+      Section(
+        icon: Iconsax.add_square,
+        name: "Ajouter", // Translated to French for consistency
+        onTap: () {
+          // TODO: Implement 'Add' functionality
+          debugPrint("Add section tapped!");
+        },
+      ),
+      Section(
+        icon: Iconsax.pen_add, // Iconsax.edit for more general 'edit'
+        name: "Modifier", // Translated to French
+        onTap: () {
+          // TODO: Implement 'Modify' functionality
+          debugPrint("Modify section tapped!");
+        },
+      ),
+    ];
   }
 }
